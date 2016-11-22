99d261765776424b763699b2258b48ea0eb2e31f
Illia <qwerty@mail.ru>
2016-11-22 12:42:39 +0200

    Added SpellCaster and SpellCasterStateClasses

---
+++ Abilities/SpellCasterState.h
@@ -0,0 +1,21 @@
+#ifndef SPELL_CASTER_STATE_H
+#define SPELL_CASTER_STATE_H
+
+
+class SpellCasterState {
+    protected:
+        int mannaPoints;
+        int mannaPointsLimit;
+        double combatSpellRate;
+        double supportSpellRate;
+
+    public:
+        SpellCasterState(int mannaPointsLimit, double combatSpellRate, double supportSpellRate);
+
+        int getMannaPoints() const;
+        double getCombatSpellRate() const;
+        double getSupportSpellRate() const;
+};
+
+
+#endif //SPELL_CASTER_STATE_H
